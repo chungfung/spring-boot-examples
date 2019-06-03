42115671b6155239df0713112b962e5e445b1703
chungfung <fy_java@163.com>
2019-06-03 10:10:54 +0800

    spring boot thymeleaf

--- spring-boot-thymeleaf/src/main/java/com/chungfung/controller/UserController.java
+++ spring-boot-thymeleaf/src/main/java/com/chungfung/controller/UserController.java
@@ -1,10 +1,14 @@
 package com.chungfung.controller;
 
+import com.chungfung.model.Tree;
 import com.chungfung.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @Description
@@ -58,5 +62,44 @@ public class UserController {
     public String switchcase(ModelMap map) {
         map.addAttribute("sex", "woman");
         return "switch";
+    }
+
+    @RequestMapping("/table")
+    public String table(ModelMap map) {
+        Tree root = new Tree();
+        root.setId("1");
+        root.setName("跟目录");
+        root.setPid("0");
+
+        Tree level1 = new Tree();
+        level1.setId("2");
+        level1.setName("子目录1");
+        level1.setPid("1");
+
+        Tree level2 = new Tree();
+        level2.setId("3");
+        level2.setName("子目录2");
+        level2.setPid("1");
+
+        Tree level11 = new Tree();
+        level11.setId("4");
+        level11.setName("层级三1");
+        level11.setPid("2");
+
+        Tree level12 = new Tree();
+        level12.setId("5");
+        level12.setName("层级三2");
+        level12.setPid("2");
+
+        List<Tree> list = new ArrayList<>();
+        list.add(root);
+        list.add(level1);
+        list.add(level2);
+        list.add(level11);
+        list.add(level12);
+
+        map.put("list",list);
+
+        return "table";
     }
 }
---
+++ spring-boot-thymeleaf/src/main/java/com/chungfung/model/Tree.java
@@ -0,0 +1,17 @@
+package com.chungfung.model;
+
+import lombok.Data;
+
+/**
+ * @Description
+ * @Author 丰涌
+ * @Date 2019/6/3 09:48
+ * @Version 1.0
+ */
+@Data
+public class Tree {
+
+    private String id;
+    private String name;
+    private String pid;
+}
