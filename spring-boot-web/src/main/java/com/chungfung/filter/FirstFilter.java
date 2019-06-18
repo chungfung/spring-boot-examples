3765142f344d3bdbc4bd1b0e39eba8b53b1e42ae
chungfung <fy_java@163.com>
2019-06-18 18:15:12 +0800

    spring boot web整合servlet、listener、filter、文件上传

---
+++ spring-boot-web/src/main/java/com/chungfung/filter/FirstFilter.java
@@ -0,0 +1,33 @@
+package com.chungfung.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+/**
+ * @Description
+ * @Author 丰涌
+ * @Date 2019/6/18 17:38
+ * @Version 1.0
+ */
+@WebFilter(filterName="FirstFilter",urlPatterns="/first")
+public class FirstFilter implements Filter {
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse,
+                         FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("执行filter前操作！");
+        filterChain.doFilter(servletRequest,servletResponse);
+        System.out.println("执行filter后操作！");
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
---
+++ spring-boot-web/src/main/java/com/chungfung/listener/FirstListener.java
@@ -0,0 +1,25 @@
+package com.chungfung.listener;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
+
+/**
+ * @Description
+ * @Author 丰涌
+ * @Date 2019/6/18 17:41
+ * @Version 1.0
+ */
+@WebListener
+public class FirstListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized(ServletContextEvent servletContextEvent) {
+        System.out.println("listener初始化执行！");
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent) {
+        System.out.println("listener销毁后执行！");
+    }
+}
